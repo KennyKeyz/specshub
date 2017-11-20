accf3f9005e6b7e7bfc9e1a66a3da2090db22644
vagrant <vagrant@precise32.(none)>
2017-11-20 16:09:32 +0000

    secret key base

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module SpecsHub
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
